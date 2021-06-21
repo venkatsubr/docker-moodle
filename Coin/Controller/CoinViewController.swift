d4f68f4a4a7ed7c68cc2cf1834e2bc32df56b216
venkatsubr <91278229+venkatsubr@users.noreply.github.com>
2021-06-21 09:35:09 +0000

    fixed case

--- Coin/Controller/CoinViewController.swift
+++ Coin/Controller/CoinViewController.swift
@@ -9,3 +9,33 @@
 import UIKit
 
 class CoinViewController: UIViewController {
+
+    @IBOutlet weak var bitcoinLabel: UILabel!
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+
+    var coinManager = CoinManager()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        coinManager.delegate = self
+        currencyPicker.delegate = self
+        currencyPicker.dataSource = self
+
+    }
+}
+
+//MARK: - UIPickerViewDataSource
+
+extension CoinViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
+    }
+}
+
+//MARK: - UIPickerViewDelegate
