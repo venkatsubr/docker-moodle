dd326b560fbc825ddba0b9d9fbe750c8e568683f
venkatsubr <91278229+venkatsubr@users.noreply.github.com>
2021-08-08 10:47:06 +0000

    Release 3.1.4

--- Coin/Model/CoinManager.swift
+++ Coin/Model/CoinManager.swift
@@ -18,4 +18,7 @@ struct CoinManager {
     let apiKey: String
     var delegate: CoinManagerDelegate?
 
-    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK"
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+
+    init() {
+    
