087ff3b35762a358870638e58d7d953662981a6f
venkatsubr <91278229+venkatsubr@users.noreply.github.com>
2021-09-19 19:20:34 +0000

    Replacing tabs with spaces

--- Coin/Model/CoinManager.swift
+++ Coin/Model/CoinManager.swift
@@ -36,4 +36,5 @@ struct CoinManager {
                 }
                 if let saveData = data {
                     if let coinRate = parseJSON(saveData) {
-                        let priceString = String(format: "%.2
+                        let priceString = String(format: "%.2f", coinRate)
+                    
